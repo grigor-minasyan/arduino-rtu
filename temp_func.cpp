8f2e1ac81e6bc4c951de81fe99d9535634c317b5
Grigor Minasyan <grigorminasyan1998@gmail.com>
2020-03-06 18:44:15 +0000

    temp_func.cpp edited online with Bitbucket

--- temp_func.cpp
+++ temp_func.cpp
@@ -3,15 +3,16 @@ int8_t to_farenheit(int8_t x)  {
   return x * 1.8 + 32;
 }
 
+//this keeps track of the data and does the storing in EEPROM
 Eeprom_indexes rtc_dht_data_range(0, EEPROM.length()-1, 0, 0);
 Data_To_Store time_and_data;
 
-
+//x is the number of data to print
 void print_EEPROM_data(int x) {
   rtc_dht_data_range.print_data(x);
 }
 
-//not to be called from outside, only by the looper
+//not to be called from outside, only by the looper, limited to 0.5hz maximum
 void read_temp_hum() {
   int err = SimpleDHTErrSuccess;
   if ((err = dht22.read(&cur_temp, &cur_humidity, NULL)) != SimpleDHTErrSuccess) {
@@ -25,6 +26,7 @@ void read_temp_hum() {
 void read_temp_hum_loop() {
 	if (curr_time - prev_time_dht_short >= dht_read_short_delay) {
 		read_temp_hum();
+        //setting the max and min values
     if (cur_temp <  min_temp) min_temp = cur_temp;
     if (cur_temp > max_temp) max_temp = cur_temp;
     if (cur_humidity <  min_humidity) min_humidity = cur_humidity;
