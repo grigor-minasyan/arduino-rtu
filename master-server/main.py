87543bec1b71d0db017a31b5fa07a43fc478f4d0
Grigor Minasyan <grigorminasyan1998@gmail.com>
2020-03-29 15:32:55 -0700

    ignore compiled .pyc gitignore

--- master-server/main.py
+++ master-server/main.py
@@ -3,13 +3,6 @@
 from flask import Flask, render_template, jsonify, request
 import threading, time, socket, sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_address = ('127.0.0.1', 10000)
-
-print('starting up on %s port %s' % server_address)
-sock.bind(server_address)
-
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -18,13 +11,18 @@ app = Flask(__name__)
 # which tells the application which URL should call
 # the associated function.
 cur_temp_from_udp = '120'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('127.0.0.1', 10000)
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
 def check_incoming_traffic():
     while True:
         print('\nwaiting to receive message')
         data, address = sock.recvfrom(4096)
 
         print('received %s bytes from %s' % (len(data), address))
-        print(data)
+        print(':'.join(x.encode('hex') for x in data))
         if data:
             global cur_temp_from_udp
             cur_temp_from_udp = data
